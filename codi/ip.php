97e4a296ace46858b95cab7f6e856b7dcc79afc8
LAPTOP-ULMNNGPD\Olga <15587242.clot@fje.edu>
2022-10-11 17:13:39 +0200

    Prova de calculadora de subxarxa 1

--- codi/ip.php
+++ codi/ip.php
@@ -8,10 +8,10 @@
 			require_once(__DIR__ . '/vendor/autoload.php');
 
 			#Obtenció del primer operand
-			$sub = new IPv4\SubnetCalculator();
 
 			if ($_GET["op1"] =="") {
-				$ipAddress = $sub->getIPAddress();
+				$operand1 = 0;
+				
 			}
 			else{
 				$operand1 = $_GET["op1"];
@@ -19,15 +19,19 @@
 			#
 			#Obtenció del segon operand
 			if ($_GET["op2"] =="") {
-				$subnetMask = $sub->getSubnetMask();
+				$operand2 = 0;
 			}
 			else{
 				$operand2 = $_GET["op2"];
 			}
 			#
 			#Obtencio del resultat i mostrant el resultat
-			$resultat = $operand1 / $operand2;
-			echo "<p>El resultat de $operand1 / $operand2 és igual a $resultat</p>";			
+			$sub = new IPv4\SubnetCalculator([$_GET["op1"],$_GET["op2"]]);
+			$network= $sub->getNetworkPortion();
+			$broadcastAddress = $sub->getBroadcastAddress();
+			$addressableHostRange = $sub->getAddressHostRange();
+			echo"<p>Dirección IP: $operand1 / $operand2<p><br>"
+						
 		?>
 		<a href="ip.html">Torna a la pàgina anterior</a>
 	</body>
